b0f259980aa815d6fb6039e40e26e77cff318697
sun00j <sun00j@sina.com>
2014-09-25 00:32:57 +0800

    add java

--- src/com/sun00j/dogleg/dto/Card.java
+++ src/com/sun00j/dogleg/dto/Card.java
@@ -1,5 +1,18 @@
 package com.sun00j.dogleg.dto;
 
 public class Card {
-
+	private String title;
+	private int type;
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public int getType() {
+		return type;
+	}
+	public void setType(int type) {
+		this.type = type;
+	}
 }
--- src/com/sun00j/dogleg/dto/Person.java
+++ src/com/sun00j/dogleg/dto/Person.java
@@ -1,5 +1,25 @@
 package com.sun00j.dogleg.dto;
 
 public class Person {
-
+	private int id;
+	private String name;
+	private int score;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getScore() {
+		return score;
+	}
+	public void setScore(int score) {
+		this.score = score;
+	}
 }
