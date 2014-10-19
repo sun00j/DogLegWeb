39248dd6bf46dd5fa642efbc8032dbed666e5809
sun00j <sun00j@sina.com>
2014-10-19 22:21:06 +0800

    Login

--- src/com/sun00j/dogleg/controller/GameController.java
+++ src/com/sun00j/dogleg/controller/GameController.java
@@ -3,6 +3,8 @@ package com.sun00j.dogleg.controller;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import net.sf.json.JSONObject;
+
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.multiaction.MultiActionController;
 
@@ -12,4 +14,14 @@ public class GameController extends MultiActionController{
 		return new ModelAndView("Test.jsp");
 	}
 
+	public void Login(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		String userName = request.getParameter("name");
+		String pwd = request.getParameter("password");
+		response.setContentType("text/html; charset=GBK");
+		request.getSession(true).setAttribute("userName", userName);
+		JSONObject jsonObj = new JSONObject();
+		jsonObj.put("userName", userName);
+		jsonObj.put("ext", "Welcom");
+		response.getWriter().println(jsonObj.toString());
+	}
 }
